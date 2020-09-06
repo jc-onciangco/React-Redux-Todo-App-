4bc7ab3d97e7f9a4ef8fd666c2e7e6de5a6e3214
Uwang20 <ohjuan205@gmail.com>
2020-09-06 15:39:39 +0800

    add build dir

---
+++ build/precache-manifest.67d348ea285459c314d017dc0818944d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789ef677d699c36ef68793b54854c837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee708a1667881a40b8b7",
+    "url": "/static/css/main.8ddddd48.chunk.css"
+  },
+  {
+    "revision": "8c516468ec41410e9e1b",
+    "url": "/static/js/2.07073ca2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07073ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee708a1667881a40b8b7",
+    "url": "/static/js/main.73af1728.chunk.js"
+  },
+  {
+    "revision": "2aa6c3973ac727e9608c",
+    "url": "/static/js/runtime-main.6dd49f05.js"
+  }
+]);
